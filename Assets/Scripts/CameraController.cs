a059834a24ccec82f8542aac150369b62acdd9ac
asd13554 <natchapom33@gmail.com>
2024-01-18 10:29:56 +0700

    Clamp Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,7 +9,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Camera cam;
 
     [Header("Move")] private float moveSpeed;
-
+    
+    [SerializeField] private Transform corner1;
+    [SerializeField] private Transform corner2;
+    
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
 
@@ -42,5 +45,17 @@ public class CameraController : MonoBehaviour
         Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
 
         transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = Clamp(corner1.position, corner2.position);
     }
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x,
+                lowerLeft.x, topRight.x),
+            transform.position.y,
+            Mathf.Clamp(transform.position.z,
+                lowerLeft.z, topRight.z));
+        
+        return pos;
+    }
+
 }
