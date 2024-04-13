c0d3617f3e9274145b766b1660349341f816671f
asd13554 <natchapom33@gmail.com>
2024-04-13 23:01:52 +0700

    Add Main Menu

--- Assets/Scripts/Resources/ResourceSource.cs
+++ Assets/Scripts/Resources/ResourceSource.cs
@@ -40,7 +40,10 @@ public class ResourceSource : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        onRsrcQuantityChange.Invoke();
         
+        //onInfoQuantityChange.AddListener(delegate { InfoManager.instance.ShowEnemyAllInfo(this); });
+        //onInfoQuantityChange.AddListener((() => InfoManager.instance.ShowEnemyAllInfo(this)));
     }
 
     // Update is called once per frame
@@ -65,6 +68,8 @@ public class ResourceSource : MonoBehaviour
             amountToGive = amountRequest;
 
         quantity -= amountToGive;
+        
+        onRsrcQuantityChange.Invoke();
 
         // if we're depleted, delete the resource
         if (quantity <= 0)
